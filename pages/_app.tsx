a648d190d02c57942e9b6730bcdd61d7d7ce75a4
Hassan El Mghari <hassan4709@gmail.com>
2023-01-16 15:22:39 -0500

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
@@ -5,6 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
