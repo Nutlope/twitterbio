b7ed528ab025d3bd06349a9aa67d26581d096922
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2023-01-23 03:29:28 +0900

    handle errors

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -58,15 +58,20 @@ const handler = async (req: NextRequest): Promise<Response> => {
             controller.close();
             return;
           }
-          const json = JSON.parse(data);
-          const text = json.choices[0].text;
-          if (counter < 2 && (text.match(/\n/) || []).length) {
-            // this is a prefix character (i.e., "\n\n"), do nothing
-            return;
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].text;
+            if (counter < 2 && (text.match(/\n/) || []).length) {
+              // this is a prefix character (i.e., "\n\n"), do nothing
+              return;
+            }
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+            counter++;
+          } catch (e) {
+            // maybe parse error
+            controller.error(e);
           }
-          const queue = encoder.encode(text);
-          controller.enqueue(queue);
-          counter++;
         }
       }
 
