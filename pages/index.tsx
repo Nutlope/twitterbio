dfb580fe638de42bc6cf693be170af10dc702fa1
Ricardo Guillen I <ricardo.guillen@elaniin.com>
2023-02-01 00:06:13 -0400

    ✨ Add new "range" input field for selecting the length of the generation of the summary.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,180 +1,213 @@
-import { AnimatePresence, motion } from "framer-motion";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import { Toaster, toast } from "react-hot-toast";
-import DropDown, { VibeType } from "../components/DropDown";
-import Footer from "../components/Footer";
-import Github from "../components/GitHub";
-import Header from "../components/Header";
-import LoadingDots from "../components/LoadingDots";
-import ResizablePanel from "../components/ResizablePanel";
+import { AnimatePresence, motion } from 'framer-motion'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+import { Toaster, toast } from 'react-hot-toast'
+import DropDown, { VibeType } from '../components/DropDown'
+import Footer from '../components/Footer'
+import Github from '../components/GitHub'
+import Header from '../components/Header'
+import LoadingDots from '../components/LoadingDots'
+import ResizablePanel from '../components/ResizablePanel'
 
 const Home: NextPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Professional");
-  const [generatedBios, setGeneratedBios] = useState<String>("");
+  const [loading, setLoading] = useState(false)
+  const [summary, setSummary] = useState('')
+  const [length, setLength] = useState(42)
+  const [vibe, setVibe] = useState<VibeType>('Professional')
+  const [generatedSummaries, setGeneratedSummaries] = useState<String>('')
 
-  console.log("Streamed response: ", generatedBios);
+  console.log('Streamed response: ', generatedSummaries)
 
   const prompt =
-    vibe === "Funny"
-      ? `Generate 2 funny twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 20 words and base it on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
+    vibe === 'Funny'
+      ? `Generate 2 funny text summaries with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated summary is at max ${length} words and base it on this context: ${summary}${
+          summary.slice(-1) === '.' ? '' : '.'
         }`
-      : `Generate 2 ${vibe} twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure each generated bio is at least 14 words and at max 20 words and base them on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
-        }`;
+      : `Generate 2 ${vibe} summaries clearly labeled "1." and "2.". Make sure each generated summary is at least ${length} words limit and base them on this context: ${summary}${
+          summary.slice(-1) === '.' ? '' : '.'
+        }`
 
-  const generateBio = async (e: any) => {
-    e.preventDefault();
-    setGeneratedBios("");
-    setLoading(true);
-    const response = await fetch("/api/generate", {
-      method: "POST",
+  const generateSummary = async (e: any) => {
+    e.preventDefault()
+    setGeneratedSummaries('')
+    setLoading(true)
+    const response = await fetch('/api/generate', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         prompt,
       }),
-    });
-    console.log("Edge function returned.");
+    })
+    console.log('Edge function returned.')
 
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new Error(response.statusText)
     }
 
     // This data is a ReadableStream
-    const data = response.body;
+    const data = response.body
     if (!data) {
-      return;
+      return
     }
 
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
+    const reader = data.getReader()
+    const decoder = new TextDecoder()
+    let done = false
 
     while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
+      const { value, done: doneReading } = await reader.read()
+      done = doneReading
+      const chunkValue = decoder.decode(value)
+      setGeneratedSummaries((prev) => prev + chunkValue)
     }
 
-    setLoading(false);
-  };
+    setLoading(false)
+  }
 
   return (
-    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className='flex flex-col items-center justify-center max-w-5xl min-h-screen py-2 mx-auto'>
       <Head>
-        <title>Twitter Generator</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Text Summarizer</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+      <main className='flex flex-col items-center justify-center flex-1 w-full px-4 mt-12 text-center sm:mt-20'>
         <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='flex items-center justify-center px-4 py-2 mb-5 space-x-2 text-sm text-gray-600 transition-colors bg-white border border-gray-300 rounded-full shadow-md max-w-fit hover:bg-gray-100'
+          href='https://github.com/ryarturogi/text-sumarizer'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Github />
           <p>Star on GitHub</p>
         </a>
-        <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
-          Generate your next Twitter bio in seconds
+        <h1 className='max-w-2xl text-4xl font-bold sm:text-6xl text-slate-900'>
+          Generate a summary from a text in seconds.
         </h1>
-        <p className="text-slate-500 mt-5">18,167 bios generated so far.</p>
-        <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
+        <div className='w-full max-w-xl'>
+          <div className='flex items-center mt-10 space-x-3'>
             <Image
-              src="/1-black.png"
+              src='/1-black.png'
               width={30}
               height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
+              alt='1 icon'
+              className='mb-5 sm:mb-0'
             />
-            <p className="text-left font-medium">
-              Copy your current bio{" "}
-              <span className="text-slate-500">
-                (or write a few sentences about yourself)
+            <p className='font-medium text-left'>
+              Copy current summary{' '}
+              <span className='text-slate-500'>
+                (or write a few sentences about the topic you want to write
+                about)
               </span>
               .
             </p>
           </div>
           <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
+            value={summary}
+            onChange={(e) => setSummary(e.target.value)}
             rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            className='w-full my-5 border-gray-300 rounded-md shadow-sm focus:border-black focus:ring-black'
             placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+              'e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com.'
             }
           />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
+          <div className='flex items-center mb-5 space-x-3'>
+            <Image src='/2-black.png' width={30} height={30} alt='1 icon' />
+            <p className='font-medium text-left'>Select your vibe.</p>
           </div>
-          <div className="block">
+          <div className='block'>
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
+          </div>
+          <div className='flex items-center mt-8 mb-5 space-x-3'>
+            <Image src='/2-black.png' width={30} height={30} alt='1 icon' />
+            <p className='font-medium text-left'>
+              Select the length of your summary.
+            </p>
+
+            {/* input counter */}
+            <div className='flex items-center justify-center w-10 h-10 ml-2 text-sm text-white bg-black rounded-full'>
+              <p>{length}</p>
+            </div>
+          </div>
+
+          <div className='flex items-center justify-center w-full mt-5 space-x-3'>
+            <div className='flex items-center justify-center w-10 h-10 text-sm text-white bg-black rounded-full'>
+              <p>1</p>
+            </div>
+            <input
+              type='range'
+              min='1'
+              max='100'
+              value={length}
+              onChange={(e) => setLength(parseInt(e.target.value))}
+              className='w-full h-2 bg-gray-300 rounded-full cursor-pointer'
+            />
+            <div className='flex items-center justify-center w-10 h-10 text-sm text-white bg-black rounded-full'>
+              <p>100</p>
+            </div>
+          </div>
+          <div className='flex items-center justify-between mt-5 space-x-3'>
+            <p className='text-sm text-gray-500'>Short</p>
+            <p className='text-sm text-gray-500'>Long</p>
           </div>
 
           {!loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              className='w-full px-4 py-2 mt-8 font-medium text-white bg-black rounded-xl sm:mt-10 hover:bg-black/80'
+              onClick={(e) => generateSummary(e)}
             >
-              Generate your bio &rarr;
+              Generate summary &rarr;
             </button>
           )}
           {loading && (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className='w-full px-4 py-2 mt-8 font-medium text-white bg-black rounded-xl sm:mt-10 hover:bg-black/80'
               disabled
             >
-              <LoadingDots color="white" style="large" />
+              <LoadingDots color='white' style='large' />
             </button>
           )}
         </div>
         <Toaster
-          position="top-center"
+          position='top-center'
           reverseOrder={false}
           toastOptions={{ duration: 2000 }}
         />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+        <hr className='h-px bg-gray-700 border-1 dark:bg-gray-700' />
         <ResizablePanel>
-          <AnimatePresence mode="wait">
-            <motion.div className="space-y-10 my-10">
-              {generatedBios && (
+          <AnimatePresence mode='wait'>
+            <motion.div className='my-10 space-y-10'>
+              {generatedSummaries && (
                 <>
                   <div>
-                    <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
-                      Your generated bios
+                    <h2 className='mx-auto text-3xl font-bold sm:text-4xl text-slate-900'>
+                      Your generated summaries
                     </h2>
                   </div>
-                  <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                    {generatedBios
-                      .substring(generatedBios.indexOf("1") + 3)
-                      .split("2.")
-                      .map((generatedBio) => {
+                  <div className='flex flex-col items-center justify-center max-w-xl mx-auto space-y-8'>
+                    {generatedSummaries
+                      .substring(generatedSummaries.indexOf('1') + 3)
+                      .split('2.')
+                      .map((generatedSummary) => {
                         return (
                           <div
-                            className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                            className='p-4 transition bg-white border shadow-md rounded-xl hover:bg-gray-100 cursor-copy'
                             onClick={() => {
-                              navigator.clipboard.writeText(generatedBio);
-                              toast("Bio copied to clipboard", {
-                                icon: "✂️",
-                              });
+                              navigator.clipboard.writeText(generatedSummary)
+                              toast('summary copied to clipboard', {
+                                icon: '✂️',
+                              })
                             }}
-                            key={generatedBio}
+                            key={generatedSummary}
                           >
-                            <p>{generatedBio}</p>
+                            <p>{generatedSummary}</p>
                           </div>
-                        );
+                        )
                       })}
                   </div>
                 </>
@@ -185,7 +218,7 @@ const Home: NextPage = () => {
       </main>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
