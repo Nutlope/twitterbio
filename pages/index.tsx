86d862ddffed3a7f0db49c47f340b9aa6b8d7fe0
Hassan El Mghari <hassan4709@gmail.com>
2023-03-01 22:49:43 -0500

    changed prompt to match chatGPT style

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,14 +17,14 @@ const Home: NextPage = () => {
   const [vibe, setVibe] = useState<VibeType>("Professional");
   const [generatedBios, setGeneratedBios] = useState<String>("");
 
-  const prompt =
+  const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
     vibe === "Funny"
-      ? `Generate 2 funny twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 20 words and base it on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
-        }`
-      : `Generate 2 ${vibe} twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure each generated bio is at least 14 words and at max 20 words and base them on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
-        }`;
+      ? "Make sure there is a joke in there and it's a little ridiculous."
+      : null
+  }
+      Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
+    bio.slice(-1) === "." ? "" : "."
+  }`;
 
   const generateBio = async (e: any) => {
     e.preventDefault();
