0b849fc94c056607d9c21cfce004889a6411b895
Markus Flodmark <markus.flodmark@brinkcommerce.com>
2023-03-15 20:51:57 +0100

    fix max tokens

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -24,7 +24,7 @@ const handler = async (req: Request): Promise<Response> => {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 200,
+    max_tokens: 2000,
     stream: true,
     n: 1,
   };
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,9 +26,9 @@ const Home: NextPage = () => {
     }
   };
 
-  const promptSWOT = `Generate a SWOT analysis of company ${company}, divided by "Strenghts:", "Weaknesses:", "Opportunities:" and "Threats:". Each section have top 1-5 points summarized of max 125 characters. If no company is found return nothing.`;
+  const promptSWOT = `Generate a SWOT analysis of company ${company}, divided by "Strenghts:", "Weaknesses:", "Opportunities:" and "Threats:". Each section have top 1-5 points summarized of max 300 characters. If no company is found return nothing.`;
 
-  const promptColor = `Max 500 characters. Give some context on company ${company} `;
+  const promptColor = `Max 1000 characters. Give some context on company ${company} `;
 
   const generateSWOT = async (e: any) => {
     e.preventDefault();
