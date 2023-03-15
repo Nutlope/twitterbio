d1701eadc0e17701dbf16e1181e80d3e9d89a959
Markus Flodmark <markus.flodmark@brinkcommerce.com>
2023-03-15 21:23:29 +0100

    refactor generate color

---
+++ components/GenerateColor.tsx
@@ -0,0 +1,59 @@
+import LoadingDots from "../components/LoadingDots";
+
+export default function GenerateColor(props: any) {
+  const {
+    generatedSWOT,
+    loading,
+    loadingColor,
+    color,
+    swotRef,
+    strengthsStyle,
+    weaknessesStyle,
+    opportunitiesStyle,
+    threatsStyle,
+    generateColor,
+  } = props;
+
+  return (
+    <div className="space-y-10 my-10">
+      {generatedSWOT && !loading && !generatedSWOT.includes("SWOT") && (
+        <>
+          <div>
+            <h2
+              className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+              ref={swotRef}
+            >
+              Click for more color
+            </h2>
+          </div>
+          <div className="grid grid-rows-2 md:grid-rows-1 grid-flow-col gap-4 mx-auto">
+            {["Strengths", "Weaknessess", "Opportunities", "Threats"].map(
+              (c, index) => {
+                return (
+                  <div
+                    style={
+                      index == 0
+                        ? strengthsStyle
+                        : index == 1
+                        ? weaknessesStyle
+                        : index == 2
+                        ? opportunitiesStyle
+                        : threatsStyle
+                    }
+                    className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-pointer border text-left"
+                    onClick={(e) => generateColor(e, c)}
+                  >
+                    <h2 className="font-bold text-center">{c}</h2>
+                    {loadingColor && color === c && (
+                      <LoadingDots color="black" style="large" />
+                    )}
+                  </div>
+                );
+              }
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { Toaster } from "react-hot-toast";
 import { useRef, useState } from "react";
 import GeneratedSwot from "../components/GeneratedSwot";
 import GeneratedColor from "../components/GeneratedColor";
+import GenerateColor from "../components/GenerateColor";
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
@@ -193,46 +194,18 @@ const Home: NextPage = () => {
           opportunitiesStyle={opportunitiesStyle}
           threatsStyle={threatsStyle}
         ></GeneratedSwot>
-        <div className="space-y-10 my-10">
-          {generatedSWOT && !loading && !generatedSWOT.includes("SWOT") && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={swotRef}
-                >
-                  Click for more color
-                </h2>
-              </div>
-              <div className="grid grid-rows-2 md:grid-rows-1 grid-flow-col gap-4 mx-auto">
-                {["Strengths", "Weaknessess", "Opportunities", "Threats"].map(
-                  (c, index) => {
-                    return (
-                      <div
-                        style={
-                          index == 0
-                            ? strengthsStyle
-                            : index == 1
-                            ? weaknessesStyle
-                            : index == 2
-                            ? opportunitiesStyle
-                            : threatsStyle
-                        }
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-pointer border text-left"
-                        onClick={(e) => generateColor(e, c)}
-                      >
-                        <h2 className="font-bold text-center">{c}</h2>
-                        {loadingColor && color === c && (
-                          <LoadingDots color="black" style="large" />
-                        )}
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-            </>
-          )}
-        </div>
+        <GenerateColor
+          generatedSWOT={generatedSWOT}
+          loadingColor={loadingColor}
+          loading={loading}
+          generateColor={generateColor}
+          color={color}
+          strengthsStyle={strengthsStyle}
+          weaknessesStyle={weaknessesStyle}
+          opportunitiesStyle={opportunitiesStyle}
+          threatsStyle={threatsStyle}
+          swotRef={swotRef}
+        ></GenerateColor>
         <GeneratedColor
           generatedColor={generatedColor}
           company={company}
