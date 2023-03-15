41c8e65298390c87dad136662a92e506c5289201
Markus Flodmark <markus.flodmark@brinkcommerce.com>
2023-03-15 10:38:58 +0100

    fix margin on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-2 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
         Powered by{" "}
         <a
