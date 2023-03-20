fb6b99a349bf1d9fc09c17c9d7eff84aeee9704c
meerkat0688 <78197797+meerkat0688@users.noreply.github.com>
2023-03-20 13:11:23 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,9 @@ const Home: NextPage = () => {
     }
   };
 
-  const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
+  const prompt = `paraphrase below sentence: ${vibe} . ${
     vibe === "Funny"
-      ? "Make sure there is a joke in there and it's a little ridiculous."
+      ? "Make sure there is a fluency sentence."
       : null
   }
       Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
