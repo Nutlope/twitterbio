bee0ff8d059161b139f3675aa9021a2efaaaa416
Jaron Heard <jaronheard@gmail.com>
2023-10-03 13:39:54 -0700

    use fathom analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
+
 import "../styles/globals.css";
 
 const title = "timetime.cc";
@@ -28,12 +31,33 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load(process.env.FATHOM_TRACKING_CODE || "", {
+      includedDomains: ["timetime.cc", "www.timetime.cc"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <html lang="en">
-      <body>
-        {children}
-        <Analytics />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
