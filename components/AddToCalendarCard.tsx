51a32c2d9ea15d04f7351637f48b11de42c60e82
Jaron Heard <jaronheard@gmail.com>
2023-10-11 14:16:30 -0700

    add optional source link (#4)

--- components/AddToCalendarCard.tsx
+++ components/AddToCalendarCard.tsx
@@ -22,12 +22,15 @@ export function AddToCalendarCard({
   const [startTime, setStartTime] = useState(initialProps.startTime);
   const [endDate, setEndDate] = useState(initialProps.endDate);
   const [endTime, setEndTime] = useState(initialProps.endTime);
+  const [link, setLink] = useState("");
 
   const updatedProps = {
     ...initialProps,
     name,
     location,
-    description,
+    description: link
+      ? description + "\n\n" + `[url]${link}|Source[/url]`
+      : description,
     startDate,
     startTime,
     endDate,
@@ -142,6 +145,26 @@ export function AddToCalendarCard({
             defaultValue={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+        </div>
+      </div>
+      <div className="col-span-full">
+        <label
+          htmlFor="location"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Source Link (optional)
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input
+              type="url"
+              name="link"
+              id="link"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </div>
         </div>
       </div>
     </div>
