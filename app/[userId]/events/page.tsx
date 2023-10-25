f4f75987e1fd903f49d1fdb8b7e1bbaef5e7fa10
Jaron Heard <jaronheard@gmail.com>
2023-10-25 14:29:44 -0700

    add past events (#14)

--- app/[userId]/events/page.tsx
+++ app/[userId]/events/page.tsx
@@ -5,6 +5,12 @@ import { db } from "../../../lib/db";
 import Footer from "../../../components/Footer";
 import { clerkClient } from "@clerk/nextjs";
 import { UserInfo } from "../../../components/UserInfo";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "../../../components/Accordian";
 
 export default async function Page({ params }: { params: { userId: string } }) {
   const events = await db.event.findMany({
@@ -15,6 +21,7 @@ export default async function Page({ params }: { params: { userId: string } }) {
       id: true,
       event: true,
       createdAt: true,
+      startDateTime: true,
     },
     orderBy: {
       startDateTime: "asc",
@@ -22,6 +29,12 @@ export default async function Page({ params }: { params: { userId: string } }) {
   });
 
   const user = await clerkClient.users.getUser(params.userId);
+
+  const pastEvents = events.filter((item) => item.startDateTime < new Date());
+
+  const futureEvents = events.filter(
+    (item) => item.startDateTime >= new Date()
+  );
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
@@ -32,21 +45,62 @@ export default async function Page({ params }: { params: { userId: string } }) {
           <UserInfo user={user} />
         </div>{" "}
         <div className="p-2"></div>
-        {events.length === 0 ? (
-          <p className="text-gray-500 text-lg">No events found.</p>
-        ) : (
-          <ul role="list" className="divide-y divide-gray-100 max-w-full">
-            {events.map((item) => (
-              <EventCard
-                key={item.id}
-                userId={params.userId}
-                id={item.id}
-                event={item.event as AddToCalendarButtonProps}
-                createdAt={item.createdAt}
-              />
-            ))}
-          </ul>
-        )}
+        <Accordion type="multiple" className="w-full" defaultValue={["item-2"]}>
+          <AccordionItem value="item-1" className="px-6 opacity-80">
+            <AccordionTrigger>
+              <div className="flex gap-1.5">
+                Past events
+                <span className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-slate-100 bg-gray-600 rounded-full">
+                  {pastEvents.length}
+                </span>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="-mx-6">
+              {pastEvents.length === 0 ? (
+                <p className="text-gray-500 text-lg">No past events.</p>
+              ) : (
+                <ul role="list" className="divide-y divide-gray-100 max-w-full">
+                  {pastEvents.map((item) => (
+                    <EventCard
+                      key={item.id}
+                      userId={params.userId}
+                      id={item.id}
+                      event={item.event as AddToCalendarButtonProps}
+                      createdAt={item.createdAt}
+                    />
+                  ))}
+                </ul>
+              )}
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2" className="px-6">
+            <AccordionTrigger>
+              <div className="flex gap-1.5">
+                Upcoming events
+                <span className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-slate-100 bg-gray-600 rounded-full">
+                  {futureEvents.length}
+                </span>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="-mx-6">
+              {futureEvents.length === 0 ? (
+                <p className="text-gray-500 text-lg">No future events.</p>
+              ) : (
+                <ul role="list" className="divide-y divide-gray-100 max-w-full">
+                  {futureEvents.map((item) => (
+                    <EventCard
+                      key={item.id}
+                      userId={params.userId}
+                      id={item.id}
+                      event={item.event as AddToCalendarButtonProps}
+                      createdAt={item.createdAt}
+                    />
+                  ))}
+                </ul>
+              )}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </main>
       <Footer />
     </div>
