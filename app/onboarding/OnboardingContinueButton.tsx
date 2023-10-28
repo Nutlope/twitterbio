c6765878efe6c39f0325c14eca1f019e6cb7780a
Jaron Heard <jaronheard@gmail.com>
2023-10-27 18:11:12 -0700

    remove as button

--- app/onboarding/OnboardingContinueButton.tsx
+++ app/onboarding/OnboardingContinueButton.tsx
@@ -32,7 +32,6 @@ export default function OnboardingContinueButton() {
         <Link
           href={signedInPath}
           className="mt-12 block w-full rounded-xl bg-black px-4 py-2 text-center font-medium text-white hover:bg-black/80 sm:mt-16"
-          as="button"
         >
           Continue &rarr;
         </Link>
