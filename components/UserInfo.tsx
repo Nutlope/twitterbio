53d3924b8658dd55f1c1493ccc929ccb6db773f6
Jaron Heard <jaronheard@gmail.com>
2023-10-27 18:16:21 -0700

    make optional user image if no imageUrl

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -11,15 +11,17 @@ export function UserInfo(props: { user: User }) {
   return (
     <div className="group block shrink-0">
       <div className="flex items-center">
-        <div>
-          <Image
-            className="inline-block h-9 w-9 rounded-full"
-            src={props.user.imageUrl}
-            alt=""
-            width={375}
-            height={375}
-          />
-        </div>
+        {props.user.imageUrl && (
+          <div>
+            <Image
+              className="inline-block h-9 w-9 rounded-full"
+              src={props.user.imageUrl}
+              alt=""
+              width={375}
+              height={375}
+            />
+          </div>
+        )}
         <div className="ml-3">
           <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
             {props.user.firstName} {props.user.lastName}
