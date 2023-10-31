0cb9a2aeff1d14435d8ba14f2e9dace673767406
Jaron Heard <jaronheard@gmail.com>
2023-10-31 13:20:54 -0700

    fixes

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -26,11 +26,11 @@ export default function DropDown({ vibe, setVibe }: DropDownProps) {
         <Menu.Button className="inline-flex w-full items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
           {vibe}
           <ChevronUpIcon
-            className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
+            className="ui-open:hidden -mr-1 ml-2 h-5 w-5"
             aria-hidden="true"
           />
           <ChevronDownIcon
-            className="-mr-1 ml-2 hidden h-5 w-5 ui-open:block"
+            className="ui-open:block -mr-1 ml-2 hidden h-5 w-5"
             aria-hidden="true"
           />
         </Menu.Button>
