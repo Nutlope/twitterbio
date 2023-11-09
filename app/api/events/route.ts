cb924bbbf7428e04fd4b4d424e60538c4cbeb8c3
Jaron Heard <jaronheard@gmail.com>
2023-11-09 11:53:41 -0800

    log

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -20,9 +20,9 @@ const eventDeleteSchema = z.object({
 });
 
 const devLog = (message: any, ...optionalParams: any[]) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(message, ...optionalParams);
-  }
+  // if (process.env.NODE_ENV === "development") {
+  console.log(message, ...optionalParams);
+  // }
 };
 
 export async function GET() {
