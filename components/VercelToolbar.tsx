cad7c1913cacd930ded56af870a1fb3646efac91
Jaron Heard <jaronheard@gmail.com>
2023-11-16 23:01:49 -0800

    add toolbar for users with toolbar role (#61)

--- components/VercelToolbar.tsx
+++ components/VercelToolbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
 import { VercelToolbar as Toolbar } from "@vercel/toolbar/next";
 
 export function VercelToolbar() {
-  // const isEmployee = useIsEmployee();
+  const { user } = useUser();
+  const roles = user?.unsafeMetadata.roles as string[] | undefined;
+  const toolbar = roles?.includes("toolbar");
   const isDev = process.env.NODE_ENV === "development";
-  const showToolbar = isDev;
+  const showToolbar = isDev || toolbar;
   return showToolbar ? <Toolbar /> : null;
 }
